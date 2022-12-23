a0512e1fb3b59dd0b2d2294b9eb323d93ab4d959
yoonsele <yoonsele@student.42seoul.kr>
2022-12-23 10:55:59 +0900

    1223 update main with for loop

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:45:03 by yoonsele          #+#    #+#             */
-/*   Updated: 2022/12/23 09:32:16 by yoonsele         ###   ########.fr       */
+/*   Updated: 2022/12/23 10:52:50 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,12 @@ int	main(int ac, char **av)
 
 	(void)ac;
 	fd = open(av[1], O_RDONLY);
-	res = get_next_line(fd);
-	printf("%s\n", res);
-	free(res);
-
+	
+	for (int i = 0; i < 4; i++)
+	{
+		res = get_next_line(fd);
+		printf("%s\n", res);
+		free(res);
+	}
 	return (0);
 }
--- main_bonus.c
+++ main_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:45:03 by yoonsele          #+#    #+#             */
-/*   Updated: 2022/12/23 09:31:32 by yoonsele         ###   ########.fr       */
+/*   Updated: 2022/12/23 10:53:58 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,17 @@ int	main(int ac, char **av)
 	(void)ac;
 	fd1 = open(av[1], O_RDONLY);
 	fd2 = open(av[2], O_RDONLY);
-	res1 = get_next_line(fd1);
-	printf("%s\n", res1);
-	free(res1);
-
-	res2 = get_next_line(fd2);
-	printf("%s\n", res2);
-	free(res2);
+	for (int i = 0; i < 4; i++)
+	{
+		res1 = get_next_line(fd1);
+		printf("%s\n", res1);
+		free(res1);
+	}
+	for (int i = 0; i < 4; i++)
+	{
+		res2 = get_next_line(fd2);
+		printf("%s\n", res2);
+		free(res2);
+	}
 	return (0);
 }
