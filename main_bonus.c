55172d37747dc5edd87ae3b06ae403c92d1e2dce
yoonsele <yoonsele@student.42seoul.kr>
2022-12-23 09:34:24 +0900

    1223make main_bonus

--- main_bonus.c
+++ main_bonus.c
@@ -6,24 +6,30 @@
 /*   By: yoonsele <yoonsele@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:45:03 by yoonsele          #+#    #+#             */
-/*   Updated: 2022/12/23 09:32:16 by yoonsele         ###   ########.fr       */
+/*   Updated: 2022/12/23 09:31:32 by yoonsele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
 #include <fcntl.h>
 
 int	main(int ac, char **av)
 {
-	char	*res;
-	int		fd;
+	char	*res1;
+	char	*res2;
+	int		fd1;
+	int		fd2;
 
 	(void)ac;
-	fd = open(av[1], O_RDONLY);
-	res = get_next_line(fd);
-	printf("%s\n", res);
-	free(res);
+	fd1 = open(av[1], O_RDONLY);
+	fd2 = open(av[2], O_RDONLY);
+	res1 = get_next_line(fd1);
+	printf("%s\n", res1);
+	free(res1);
 
+	res2 = get_next_line(fd2);
+	printf("%s\n", res2);
+	free(res2);
 	return (0);
 }
